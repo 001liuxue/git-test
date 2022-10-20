a3cfdfb5fa73a6008034233f5f8430974c39fd66
xiejunlong <2413987421@qq.com>
2022-10-20 11:09:32 +0800

    second commit

---
+++ src/main/java/com/xie/GitTest.java
@@ -0,0 +1,8 @@
+package com.xie;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+        System.out.println("Hello.Git!");
+    }
+}
