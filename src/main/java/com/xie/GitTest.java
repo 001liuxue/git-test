26f60a77564cabb67dbbd986f0c594d8a87ad598
001liuxue <116175556+001liuxue@users.noreply.github.com>
2022-10-20 12:00:26 +0800

    pull commit

--- src/main/java/com/xie/GitTest.java
+++ src/main/java/com/xie/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master branch");
         System.out.println("hot-fix branch");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
